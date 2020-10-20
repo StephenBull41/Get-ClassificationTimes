a960ce3abe9e36bec77ab5114d55b26d106f8220
StephenBull41 <46417525+StephenBull41@users.noreply.github.com>
2020-10-20 22:55:05 +1100

    Update Get-ClassificationTimes.ps1
    Played a little golf

--- Get-ClassificationTimes.ps1
+++ Get-ClassificationTimes.ps1
@@ -1,17 +1,10 @@
-﻿Class DriverTimes {
-
+Class DriverTimes {
     [string]$driver
     [string]$best
-
+    DriverTimes([String]$a,[String]$b){$this.driver = $a; $this.best = $b}
 }
-
 $driverList = @()
-foreach($driver in ((Curl -Uri "https://hotlap-api.herokuapp.com/classification/3").Content | ConvertFrom-Json)){
-
-    $d = [DriverTimes]::new()
-    $d.driver = $driver.name
-    $d.best = ([DateTime]::MinValue).AddMilliseconds($driver.best).ToString("mm:ss.fff")
-    $driverList += $d
-}
+foreach($driver in ((Curl -Uri "https://hotlap-api.herokuapp.com/classification/3").Content | ConvertFrom-Json)){$driverList += [DriverTimes]::new($driver.name, ([DateTime]::MinValue).AddMilliseconds($driver.best).ToString("mm:ss.fff"))}
 $driverList | Sort-Object best | Format-Table
+Write-Host $driverList.Count "drivers`r`nCut off approx" (($driverList | Sort-Object best)[[Math]::Floor(0.83 * $driverList.Count)]).best -ForegroundColor Cyan
 Read-Host
