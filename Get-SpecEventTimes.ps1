36a08f8e9d67e6df382b48961f803b082009094c
StephenBull41 <46417525+StephenBull41@users.noreply.github.com>
2020-10-20 19:42:43 +1100

    Add files via upload

---
+++ Get-SpecEventTimes.ps1
@@ -0,0 +1,17 @@
+﻿Class DriverTimes {
+
+    [string]$driver
+    [string]$best
+
+}
+
+$driverList = @()
+foreach($driver in ((Curl -Uri "https://hotlap-api.herokuapp.com/classification/3").Content | ConvertFrom-Json)){
+
+    $d = [DriverTimes]::new()
+    $d.driver = $driver.name
+    $d.best = ([DateTime]::MinValue).AddMilliseconds($driver.best).ToString("mm:ss.fff")
+    $driverList += $d
+}
+$driverList | Sort-Object best | Format-Table
+Read-Host
